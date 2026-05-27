e1cffcde06c5dbb44511b4712cf26bc2ebda3ffe
yashjain1290 <yashjain99052@gmail.com>
2026-05-28 02:09:06 +0530

    Fix: HTTPS callback URLs for production

--- server.js
+++ server.js
@@ -48,7 +48,9 @@ app.use(passport.session())
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: '/api/auth/google/callback'
+  callbackURL: process.env.NODE_ENV === 'production' 
+  ? `${process.env.FRONTEND_URL}/api/auth/google/callback`
+  : 'http://localhost:3000/api/auth/google/callback'
 }, async (accessToken, refreshToken, profile, done) => {
   try {
     let user = await User.findOne({ googleId: profile.id })
